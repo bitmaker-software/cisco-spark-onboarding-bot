0b31ec454c6471f1fb3e2f4c3559edd2d19c146d
André Ventura <andre.ventura@bitmaker-software.com>
2017-05-10 18:57:32 +0100

    Simulate some work on the backend when saving the token; change/disable the button while saving

--- backend/routes/manager.js
+++ backend/routes/manager.js
@@ -2,16 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET manager. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('manager', {title: 'Onboarding manager'});
 });
 
-router.post('/api/saveToken', function(req, res, next) {
-  var token = req.body.token;
-  if (token) {
-  res.send('OK, saved token ' + token);
-  } else {
-    res.send('No token provided')
+router.post('/api/saveToken', function (req, res, next) {
+  // Simulate some work
+  setTimeout(sendResponse, 2000);
+
+  function sendResponse() {
+    var token = req.body.token;
+    if (token) {
+      res.send('OK, saved token ' + token);
+    } else {
+      res.send('No token provided')
+    }
   }
 });
 
