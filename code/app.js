9abf02b4ebe278cd8599483d592fc5d949749c1d
André Ventura <andre.ventura@bitmaker-software.com>
2017-05-19 16:59:17 +0100

    Add Babel (compiler), pg (PostgreSQL client), Sequelize (node.js ORM) and nodemon (monitor script)

--- code/app.js
+++ code/app.js
@@ -13,46 +13,46 @@ var botWebhooks = require('./bot/components/routes/incoming_webhooks');
 
 var bot = require('./bot/bot');
 
-var webserver = express();
+var app = express();
 
 // view engine setup
-webserver.set('views', path.join(__dirname, 'views'));
-webserver.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-webserver.use(logger('dev'));
-webserver.use(bodyParser.json());
-webserver.use(bodyParser.urlencoded({ extended: false }));
-webserver.use(cookieParser());
-webserver.use(sassMiddleware({
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   indentedSyntax: true, // true = .sass and false = .scss
   sourceMap: true
 }));
-webserver.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-webserver.use('/', index);
-webserver.use('/manager', manager);
-webserver.use('/webhooks', webhooks);
+app.use('/', index);
+app.use('/manager', manager);
+app.use('/webhooks', webhooks);
 
 // import all the pre-defined bot routes that are present in /bot/components/routes
 var normalizedPath = require("path").join(__dirname, "bot/components/routes");
 require("fs").readdirSync(normalizedPath).forEach(function(file) {
   console.log(file);
-  require("./bot/components/routes/" + file)(webserver, bot);
+  require("./bot/components/routes/" + file)(app, bot);
 });
 
 // catch 404 and forward to error handler
-webserver.use(function(req, res, next) {
+app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-webserver.use(function(err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -62,4 +62,4 @@ webserver.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = webserver;
+module.exports = app;
