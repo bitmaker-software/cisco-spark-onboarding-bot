c9dc17a189c072c2a5860872b865103b0489003e
André Ventura <andre.ventura@bitmaker-software.com>
2017-05-19 10:47:24 +0100

    Some experiments on the interface

--- code/public/javascripts/manager.js
+++ code/public/javascripts/manager.js
@@ -1,6 +1,29 @@
 $(function () {
 
   //
+  // Get data
+  //
+  $.get('/manager/api/flow/12345', {}, function (flow) {
+    flow.steps.forEach(function (step) {
+      $('#flow-container').append(
+        '<div class="step">' +
+        '<h3>' + getStepTypeNameFromId(step.step_type).description + '</h3>' +
+        '<span style="padding: 0 10px 0 10px">' + step.id + '</span>' +
+        '<span style="text-decoration: dotted">' + step.text + '</span>' +
+        '</div>'
+      )
+    });
+  });
+
+  function getStepTypeNameFromId(stepId) {
+    for (var i = 0; i < stepTypes.length; i++) {
+      if (stepTypes[i].id === stepId) {
+        return stepTypes[i];
+      }
+    }
+  }
+
+  //
   // Bot token
   //
   var button = {
--- code/routes/manager.js
+++ code/routes/manager.js
@@ -3,8 +3,54 @@ var router = express.Router();
 
 /* GET manager. */
 router.get('/', function (req, res, next) {
-  res.render('manager', {title: 'Onboarding manager'});
+  res.render('manager', {
+    title: 'Onboarding manager',
+    flows: getFlows(),
+    stepTypes: getStepTypes()
+  });
 });
+
+function getFlows() {
+  // Returns flows for the logged in tenant
+  // TODO
+
+  // Dummy data
+  return [
+    {id: 0, name: 'First flow', status: 'The status'},
+    {id: 1, name: 'Second flow', status: 'The status'},
+    {id: 2, name: 'Third flow', status: 'The status'},
+    {id: 3, name: 'Fourth flow', status: 'The status'}
+  ];
+}
+
+function getStepTypes() {
+  // Returns step types
+  // TODO
+
+  // Dummy data
+  return [
+    {
+      id: 0,
+      description: 'Announcement Message (no response from the user to move the next step)'
+    },
+    {
+      id: 1,
+      description: 'Question Step (owner must enter a question and the confirmation word that the user must enter to move on in the Flow)'
+    },
+    {
+      id: 2,
+      description: 'Document Step (owner specifies some instructions and the end user must upload a document to complete the step)'
+    },
+    {
+      id: 3,
+      description: 'Multiple Choice Step (owner specifies a list of possible options, end user replies with 1,2,3 or 4 etc)'
+    },
+    {
+      id: 4,
+      description: 'Docusign Step (owner specifies a document which will trigger a document to be sent to Docusign for the end user to digitally sign)'
+    }
+  ];
+}
 
 router.post('/api/saveToken', function (req, res, next) {
   // Simulate some work
@@ -18,6 +64,29 @@ router.post('/api/saveToken', function (req, res, next) {
       res.send('No token provided')
     }
   }
+});
+
+router.get('/api/flow/:id', function (req, res, next) {
+  // Returns the flow steps
+  // TODO
+
+  // Dummy data
+  res.send({
+    flow_id: req.param('id'),
+    steps: [
+      {id: '0', step_order: '1', text: 'Step number one', step_type: 0},
+      {id: '1', step_order: '2', text: 'Step number two', step_type: 1},
+      {id: '2', step_order: '3', text: 'Step number three', step_type: 0},
+      {id: '3', step_order: '4', text: 'Step number four', step_type: 2},
+      {id: '4', step_order: '5', text: 'Step number five', step_type: 1},
+      {id: '5', step_order: '6', text: 'Step number six', step_type: 3},
+      {id: '6', step_order: '7', text: 'Step number seven', step_type: 4}
+    ]
+  });
+});
+
+router.get('/api/flows', function (req, res, next) {
+  res.send(['first flow', 'second flow', 'third flow', 'fourth flow']);
 });
 
 module.exports = router;
