502c064b5ff3f0a59bb43011a40806b3bbcfa8ab
tiago fernandes <tiago.fernandes@bitmaker-software.com>
2017-05-21 18:17:31 +0100

    add placeholders for saving answers to the database

--- code/bot/skills/onboarding.js
+++ code/bot/skills/onboarding.js
@@ -11,21 +11,22 @@ module.exports = function (controller) {
         Retrieve the current flow from the datastore, and build the conversation accordingly
      */
     function buildConversationFromCurrentFlow(bot, message) {
-        //get from data base
+        //get the flow from the database
         var flow = retrieveCurrentFlowFromDb(bot);
+
         var thread = 'default';
         //create the conversation
         bot.createConversation(message, function(err, convo){
             flow.steps.forEach(function(step){
                 switch(step.step_type){
                     case "announcement":
-                        addAnnouncementStep(convo, step, thread);
+                        addAnnouncementStep(bot, convo, step, flow.respondent_flow_id, thread);
                         break;
                     case "free_text":
-                        addFreeTextStep(convo, step, thread);
+                        addFreeTextStep(bot, convo, step, flow.respondent_flow_id, thread);
                         break;
                     case "multiple_choice":
-                        addMultipleChoiceStep(convo, step, thread);
+                        addMultipleChoiceStep(bot, convo, step, flow.respondent_flow_id, thread);
                         break;
                     default:
                         break;
@@ -37,13 +38,9 @@ module.exports = function (controller) {
         });
     }
 
-    function addAnnouncementStep(convo, step, thread){
-/*
-        convo.addMessage(step.text, thread);
+    function addAnnouncementStep(bot, convo, step, respondent_flow_id, thread){
 
-        convo.addQuestion("Please type ok to continue", [
-*/
-        var text = 'primeira linha 1 \n\n **segunda** linha\n';
+        var text = step.text + '\n\nPlease type ok to continue';
 
         convo.addQuestion(text, [
             {
@@ -65,14 +62,18 @@ module.exports = function (controller) {
         ], {}, thread);
     };
 
-    function addFreeTextStep(convo, step, thread){
-        convo.addMessage(step.text, thread);
-        convo.addMessage("You can write as many lines as you want.")
-        convo.addQuestion("Please type @end in a single line when you're done", [
+    function addFreeTextStep(bot, convo, step, respondent_flow_id, thread){
+        var text = step.text + "\n\nYou can write as many lines as you want.\n\nPlease type @end in a single line when you're done";
+
+        convo.addQuestion(text, [
             {
                 "pattern": "^@end$",
                 "callback": function(response, convo){
-                    console.log(convo.extractResponse(step.step_id));
+                    //console.log(convo.extractResponse(step.step_id));
+                    var answer = convo.extractResponse(step.step_id);
+                    //remove the terminator
+                    answer = answer.replace("@end", "");
+                    saveTextAnswer(bot, step, respondent_flow_id, answer);
                     //save response
                     //go to next
                     convo.next();
@@ -87,28 +88,24 @@ module.exports = function (controller) {
         ], {"key": step.step_id, "multiple": true}, thread);
     }
 
-    function addMultipleChoiceStep(convo, step, thread){
-        convo.addMessage(step.text, thread);
-        var firstOption = -1;
-        var lastOption;
+    function addMultipleChoiceStep(bot, convo, step, respondent_flow_id, thread){
+        var text = step.text + '\n\n';
+
         var patternsAndCallbacks = [];
         step.choices.forEach(function(choice){
-            convo.addMessage(choice.choice_order + " - " + choice.text);
+
+            text += choice.choice_order + '. ' + choice.text + '\n\n';
 
             patternsAndCallbacks.push({
                 "pattern": "^" + choice.choice_order + "$",
                 "callback": function(response, convo){
                     //save response
+                    saveMultipleChoiceAnswer(bot, step, respondent_flow_id, choice.step_choice_id);
                     //go to next
                     convo.next();
                 }
             });
 
-            //update first and last choices indexes
-            if(firstOption == -1){
-                firstOption = choice.choice_order;
-            }
-            lastOption = choice.choice_order;
         });
 
         //add the default option
@@ -120,11 +117,28 @@ module.exports = function (controller) {
                 convo.next();
             }
         });
-        convo.addQuestion("Please choose one (" + firstOption + " - " + lastOption + ")", patternsAndCallbacks, {}, thread);
+        text += 'Please choose one option';
+        convo.addQuestion(text, patternsAndCallbacks, {}, thread);
+    }
+
+    /*
+        save an answer to the database
+     */
+    function saveTextAnswer(bot, step, respondent_flow_id, text){
+        //insert into RespondentAnswers(respondent_flow_id, step_id, text, status, answer_date) values(respondent_flow_id, step.step_id, text, 'answered', new Date());
+        console.log('saving text answer to database');
+        console.log("insert into RespondentAnswers(respondent_flow_id, step_id, text, status, answer_date) values(" + respondent_flow_id + ", " + step.step_id + ", '" + text + "', 'answered', new Date());");
+    }
+
+    function saveMultipleChoiceAnswer(bot, step, respondent_flow_id, step_choice_id){
+        //insert into RespondentAnswers(respondent_flow_id, step_id, step_choice_id, status, answer_date) values(respondent_flow_id, step.step_id, step_choice_id, 'answered', new Date());
+        console.log('saving multiple choice answer to database');
+        console.log("insert into RespondentAnswers(respondent_flow_id, step_id, step_choice_id, status, answer_date) values(" + respondent_flow_id + ", " + step.step_id + ", " + step_choice_id + ", 'answered', new Date());");
     }
 
     function retrieveCurrentFlowFromDb(bot){
         var flow = {
+            "respondent_flow_id": 345,
             "flow_id": 123,
             "name": "HR onboarding",
             "status": "running",
