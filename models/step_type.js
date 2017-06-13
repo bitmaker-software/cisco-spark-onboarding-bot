062d816f48663135df5e48e819e850da6769f0dc
André Ventura <andre.ventura@bitmaker-software.com>
2017-06-13 18:36:10 +0100

    Add the correct flow status names; remove the Docusign step type; split step type description into name and description

--- models/step_type.js
+++ models/step_type.js
@@ -2,6 +2,9 @@
 
 module.exports = function (sequelize, DataTypes) {
   return sequelize.define('step_type', {
+      name: {
+        type: DataTypes.STRING
+      },
       description: {
         type: DataTypes.STRING
       },
