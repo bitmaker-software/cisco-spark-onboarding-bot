60a3caa1605df414ac9fe0bf38b8009f5e6ac65b
Inês <gomes.inesisabel@gmail.com>
2017-07-28 16:32:48 +0100

    Primeira versão com gráficos c3

--- client/scripts/manager_flow_dashboard.js
+++ client/scripts/manager_flow_dashboard.js
@@ -1,5 +1,101 @@
 "use strict";
 
+var c3 = require('c3');
+
+let app = new Vue({
+  el: '#root',
+  data: {
+    answers : answersObj,
+    answersCategories : answersCatObj,
+    users : usersObj,
+    multipleChoices: stepChoiceObj,
+    totalUsers: sumUsers,
+  },
+  mounted: function() {
+    c3.generate({
+      bindto: '#pieChart',
+      data: {
+        columns: this.users,
+        type : 'pie',
+      },
+      title: {
+        text: 'Users Flow Status'
+      },
+      zoom: {
+        enabled: true
+      },
+      empty: {
+        label: {
+          text: "No Data"
+        }
+      },
+      /*color: {
+        pattern: ['#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c', '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5', '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f', '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5']
+      }*/
+    });
+  },
+  methods:{
+
+  }
+});
+
+let onLoad = false;
+
+let chart = c3.generate({
+  bindto: '#questionsChart',
+  data: {
+    columns: [ app.answers ],
+    type: 'bar',
+    color: function(inColor, data) {
+      let pattern = ['#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c', '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5', '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f', '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5'];
+      if(data.index !== undefined) {
+        return pattern[data.index];
+      }
+      return inColor;
+    },
+    onclick: function (d, element) {
+      if(typeof app.multipleChoices[d.index] !== 'undefined' && !onLoad)
+      {
+        onLoad = true;
+        chart.load({
+          unload: app.answers[0][0],
+          columns: app.multipleChoices[d.index],
+        });
+      }
+    },
+  },
+  legend: { hide: true  },
+  zoom: { enabled: true },
+  bar: {
+    width: {  ratio: 0.8  }
+  },
+  axis: {
+    x: {
+      type: 'category',
+      categories: app.answersCategories,
+      label: {
+        text: 'Questions',
+        position: 'outer-center'
+      }
+    }
+  },
+  title: {
+    text: 'Number of collected answers by question'
+  },
+});
+
+d3.select('#C').on('click', function(d, element) {
+  if(onLoad)
+  {
+    onLoad = false;
+    chart.load({
+      unload: true,
+      columns: [app.answers],
+    });
+  }
+});
+
+/*
 var Highcharts = require('highcharts');
 
 require('highcharts/modules/no-data-to-display')(Highcharts);
@@ -119,3 +215,4 @@ let app = new Vue({
 
     }
 });
+*/
