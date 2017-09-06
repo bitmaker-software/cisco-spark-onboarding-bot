ec35659fcc94157eecd843ef89613c118dbd3b57
André Ventura <andre.ventura@bitmaker-software.com>
2017-09-06 17:21:39 +0100

    Listen on port defined on env

--- app.config.js
+++ app.config.js
@@ -1,10 +1,11 @@
 const version = require('./package').version;
 const production = process.env.NODE_ENV === 'production';
+const port = process.env.PORT || 80; //(production ? 80 : 8080);
 
 module.exports = {
   production,
   host: '0.0.0.0', //production ? '0.0.0.0' : 'localhost',
-  port: 8080, // production ? 80 : 8080
+  port: port,
   static: {
     root: `/static/${version}`,
     suffix: production ? '.gz' : ''
